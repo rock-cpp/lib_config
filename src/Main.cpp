f817c8550fb4777db20c430f2f4ce901a52b0770
Janosch Machowinski <Janosch.Machowinski@dfki.de>
2016-01-24 14:19:00 +0100

    Moved configuration classes fro orocos_cpp into this library
    Changed namespace to libConfig.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
-#include <lib_config/Dummy.hpp>
+#include "YAMLConfiguration.hpp"
+#include <iostream>
 
 int main(int argc, char** argv)
 {
-    lib_config::DummyClass dummyClass;
-    dummyClass.welcome();
+    if(argc < 2)
+    {
+        std::cout << "Usage : cmd <configFile>" << std::endl;
+        return 0;
+    }
+    
+    libConfig::YAMLConfigParser parser;
+    
+    std::map<std::string, libConfig::Configuration> config;
+    
+    parser.loadConfigFile(argv[1], config);
 
+    std::cout << "File " << argv[1] << " contains this configurations " << std::endl;
+
+    for(const auto &it : config)
+    {
+        it.second.print();
+    }
+    
     return 0;
 }
---
+++ src/YAMLConfiguration.cpp
@@ -0,0 +1,352 @@
+#include "YAMLConfiguration.hpp"
+#include <boost/filesystem.hpp>
+#include <boost/regex.hpp>
+#include <fstream>
+#include <iostream>
+#include "Bundle.hpp"
+
+using namespace libConfig;
+
+void YAMLConfigParser::printNode(const YAML::Node &node, int level)
+{
+    for(int i = 0; i < level; i++)
+        std::cout << "  ";
+
+    switch(node.Type())
+    {
+        case YAML::NodeType::Undefined:
+            std::cout << "a Undefined Node" << std::endl;
+            break;
+        case YAML::NodeType::Scalar:
+        {
+            std::cout << "a Scalar: "<< node.as<std::string>() << std::endl;
+        }
+            break;
+        case YAML::NodeType::Sequence:
+            std::cout << "a Sequence: " << node.Tag() << std::endl;
+            break;
+        case YAML::NodeType::Map:
+            std::cout << "a Map: " << node.Tag() << std::endl;
+            displayMap(node, level + 1);
+            break;
+        case YAML::NodeType::Null:
+            break;
+    }
+}
+
+bool YAMLConfigParser::insetMapIntoArray(const YAML::Node& map, ComplexConfigValue& array)
+{
+    for(const auto &it : map)
+    {
+        std::string memberName = it.first.as<std::string>();
+//         std::cout << "Name : " << memberName << std::endl;
+        std::shared_ptr<ConfigValue> val = getConfigValue(it.second);
+        
+        if(!val)
+        {
+            std::cout << "Warning, could not get config value for " << memberName << std::endl;
+            continue;
+        }
+        
+        val->setName(memberName);
+
+        array.addValue(memberName, val);
+    }
+    return !array.getValues().empty();
+}
+
+
+bool YAMLConfigParser::insetMapIntoArray(const YAML::Node &map, ArrayConfigValue &array)
+{
+    for(const auto &it : map)
+    {
+        std::string memberName = it.first.as<std::string>();
+//         std::cout << "Name : " << memberName << std::endl;
+        std::shared_ptr<ConfigValue> val = getConfigValue(it.second);
+        
+        if(!val)
+        {
+            std::cout << "Warning, could not get config value for " << memberName << std::endl;
+            continue;
+        }
+        
+        val->setName(memberName);
+
+        array.addValue(val);
+    }
+    return !array.getValues().empty();
+}
+
+bool YAMLConfigParser::insetMapIntoArray(const YAML::Node& map, Configuration& conf)
+{
+    for(const auto &it : map)
+    {
+        std::string memberName = it.first.as<std::string>();
+//         std::cout << "Name : " << memberName << std::endl;
+        std::shared_ptr<ConfigValue> val = getConfigValue(it.second);
+        
+        if(!val)
+        {
+            std::cout << "Warning, could not get config value for " << memberName << std::endl;
+            continue;
+        }
+        
+        val->setName(memberName);
+
+        conf.addValue(memberName, val);
+    }
+    return !conf.getValues().empty();
+
+}       
+
+std::shared_ptr<ConfigValue> YAMLConfigParser::getConfigValue(const YAML::Node &node)
+{
+    switch(node.Type())
+    {
+        case YAML::NodeType::Scalar:
+        {
+            std::shared_ptr<ConfigValue> conf;
+            conf.reset(new SimpleConfigValue(node.as<std::string>()));
+            return conf;
+        }
+            break;
+        case YAML::NodeType::Sequence:
+//             std::cout << "a Sequence: " << node.Tag() << std::endl;
+            {
+                std::shared_ptr<ArrayConfigValue> values = std::make_shared<ArrayConfigValue>();
+                for(const auto it : node)
+                {
+                    std::shared_ptr<ConfigValue> curConf = getConfigValue(it);
+                    
+                    values->addValue(curConf);
+                }
+                return values;
+            }
+            break;
+        case YAML::NodeType::Map:
+        {
+//             std::cout << "a Map: " << node.Tag() << std::endl;
+            std::shared_ptr<ComplexConfigValue> mapValue = std::make_shared<ComplexConfigValue>();
+            if(!insetMapIntoArray(node, *mapValue))
+            {
+                return std::shared_ptr<ConfigValue>();
+            }
+            return mapValue;
+            break;
+        }
+        case YAML::NodeType::Undefined:
+            std::cout << "Undefined" << std::endl;
+            break;
+        case YAML::NodeType::Null:
+            std::cout << "NULL" << std::endl;
+            break;
+    }
+    return nullptr;
+}
+
+std::shared_ptr<ComplexConfigValue> YAMLConfigParser::getMap(const YAML::Node &map)
+{
+    std::shared_ptr<ComplexConfigValue> mapValue = std::make_shared<ComplexConfigValue>();
+    if(!insetMapIntoArray(map, *mapValue))
+    {
+        return nullptr;
+    }
+
+    return mapValue;
+}
+
+void YAMLConfigParser::displayMap(const YAML::Node &map, int level)
+{
+    for(const auto &it : map)
+    {
+        for(int i = 0; i < level; i++)
+            std::cout << "  ";
+        
+        std::string value = it.first.as<std::string>();
+        std::cout << "Value of first " << value << std::endl;
+        printNode(it.second, level + 1);
+    }
+    
+}
+
+bool YAMLConfigParser::loadConfigFile(const std::string& pathStr, std::map<std::string, Configuration> &subConfigs)
+{
+    subConfigs.clear();
+    using namespace boost::filesystem;
+    
+    path path(pathStr);
+
+    if(!exists(path))
+    {
+        throw std::runtime_error(std::string("Error, could not find config file ") + path.c_str());
+    }
+
+    //as this is non standard yml, we need to load and parse the config file first
+    std::ifstream fin(path.c_str());
+    std::string line;
+    
+    std::string buffer;
+    
+    Configuration curConfig("dummy");
+    bool hasConfig = false;
+
+    
+    while(std::getline(fin, line))
+    {
+        if(line.size() >= 3 && line.at(0) == '-'  && line.at(1) == '-'  && line.at(2) == '-' )
+        {
+            //found new subsection
+//             std::cout << "found subsection " << line << std::endl;
+            
+            std::string searched("--- name:");
+            if(!line.compare(0, searched.size(), searched))
+            {
+
+                if(hasConfig)
+                {
+                    try {
+                        if(!parseYAML(curConfig, applyStringVariableInsertions(buffer)))
+                            return false;
+                    } catch (std::runtime_error &e)
+                    {
+                        std::cout << "Error loading config file " << pathStr << std::endl << "    " << e.what() << std::endl;
+                        return false;
+                    }
+                    buffer.clear();
+                    subConfigs.insert(std::make_pair(curConfig.getName(), curConfig));
+                }
+                
+                hasConfig = true;
+                
+                curConfig = Configuration(line.substr(searched.size(), line.size()));
+
+//                 std::cout << "Found new configuration " << curConfig.name << std::endl;
+            } else
+            {
+                std::cout << "Error, sections must begin with '--- name:<SectionName>'" << std::endl;
+                return false;
+            }
+
+        } else
+        {
+            //line belongs to the last detected section, add it to the buffer
+            buffer.append(line);
+            buffer.append("\n");
+        }
+    }
+    
+    if(hasConfig)
+    {
+        try {
+            if(!parseYAML(curConfig, applyStringVariableInsertions(buffer)))
+                return false;
+        } catch (std::runtime_error &e)
+        {
+                        std::cout << "Error loading config file " << pathStr << std::endl << "    " << e.what() << std::endl;
+            return false;
+        }
+        subConfigs.insert(std::make_pair(curConfig.getName(), curConfig));
+    }
+
+//     for(std::map<std::string, Configuration>::const_iterator it = subConfigs.begin(); it != subConfigs.end(); it++)
+//     {
+//         std::cout << "Cur conf \"" << it->first << "\"" << std::endl;
+//         displayConfiguration(it->second);
+//     }
+    
+    return true;
+}
+
+bool YAMLConfigParser::parseYAML(Configuration& curConfig, const std::string& yamlBuffer)
+{
+    std::vector<YAML::Node> docs = YAML::LoadAll(yamlBuffer);
+
+    for(const YAML::Node &doc : docs)
+    {
+        if(doc.Type() != YAML::NodeType::Map)
+        {
+            std::cout << "Error, configurations section should only contain yml maps" << std::endl;
+            return false;
+        }
+        
+        if(doc.Type() == YAML::NodeType::Map)
+        {
+            if(!insetMapIntoArray(doc, curConfig))
+            {
+                std::cout << "Warning, could not parse config" << std::endl;
+            }
+        }
+    }
+    
+    return true;
+}
+
+std::string YAMLConfigParser::applyStringVariableInsertions(const std::string& val)
+{
+    std::string retVal;
+    std::vector<std::string> items;
+    
+    boost::regex outerRegex("<%=?\\s*(.*?)\\s*%?>");
+    
+    //Note, this needs to be defined outside of the lambdas for regex_replace
+    //if this is not the case a reference to a stack varaiable is returned, 
+    //resulting in a segfault
+    std::string ret;
+    
+    auto innerReplace = [&](const boost::smatch &innerMatch) {
+        if(!innerMatch[3].str().empty() || !innerMatch[4].str().empty() )
+        {
+            std::string var;
+            if(innerMatch[3].str().empty())
+                var = innerMatch[4];
+            else
+                var = innerMatch[3];
+            
+            char *envVal = std::getenv(var.c_str());
+            if(!envVal)
+                throw std::runtime_error("YAML Parser: Error, could not resolve environment variable " + var + " (from " + innerMatch[0] + ")");
+            //add variable to output string:
+            ret = envVal;
+            return ret;
+        }
+        
+        if(!innerMatch[6].str().empty() || !innerMatch[7].str().empty() )
+        {
+            std::string var;
+            if(innerMatch[6].str().empty())
+                var = innerMatch[7];
+            else
+                var = innerMatch[6];
+            
+            std::string file;
+            try{
+                file = Bundle::getInstance().findFile(var);
+            }
+            catch (...)
+            {
+            }
+            
+            if(file.empty())
+                throw std::runtime_error("YAML Parser: Error, could not find file " + var + " (from " + innerMatch[0] + ")");
+            //add variable to output string:
+            ret = file;
+            return ret;
+        }
+        
+        throw std::runtime_error("YAML Parser: Error, could not evaluate statement: " + innerMatch[0]);
+    };
+    
+    retVal = boost::regex_replace(val, outerRegex, [&](const boost::smatch &match) {
+        if(match.size() <= 1)
+            throw std::runtime_error("YAMLConfigParser::Error empty <% > sequence");
+        
+        std::string in = boost::regex_replace(match[1].str(), boost::regex("#\\{(.*)?\\}"), "$1");
+        
+        std::string innerMatcher("(\\[\\s*(?:\"|\')?(.*?)(?:\"|\')?\\s*\\]|\\(\\s*(?:\"|\')?(.*?)(?:\"|\')?\\s*\\))");
+        ret = boost::regex_replace(in, boost::regex("(ENV" + innerMatcher + "|(?:BUNDLES|Bundles.find_file|Bundles.find_dir)" + innerMatcher + ")"), innerReplace);
+        return ret;
+    }
+    );
+
+    return retVal;
+}
